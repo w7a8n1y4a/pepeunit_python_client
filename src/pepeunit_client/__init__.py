777b748d479cb8325f0c83ee28d99121b9258e16
Ivan Serebrennikov <admin@silberworks.com>
2025-12-06 15:37:59 +0300

    feat(version): set 1.0.0

--- src/pepeunit_client/__init__.py
+++ src/pepeunit_client/__init__.py
@@ -2,7 +2,7 @@
 PepeunitClient - Мультиплатформенная библиотека для работы с Pepeunit Unit Storage
 """
 
-__version__ = "0.10.0"
+__version__ = "1.0.0"
 
 from .client import PepeunitClient
 from .abstract_clients import AbstractPepeunitMqttClient, AbstractPepeunitRestClient
